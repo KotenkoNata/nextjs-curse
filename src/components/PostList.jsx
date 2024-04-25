8ad4eef8dc0fef11f91844d9d107f63b44604020
KotenkoNata <kotenkonata8@gmail.com>
2024-04-25 14:35:30 +0200

    feat: Adding Event Listeners

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,18 @@
 import Post from "./Post";
 import styles from "./PostList.module.css";
+import NewPost from "./NewPost";
 
 function PostList() {
+
+
     return (
-        <ul className={styles.posts}>
-            <Post author='Max' body='Awesome!' />
-            <Post author='Terry' body='Good!' />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={styles.posts}>
+                <Post author='Max' body='Awesome!' />
+                <Post author='Terry' body='Good!' />
+            </ul>
+        </>
     )
 }
 
