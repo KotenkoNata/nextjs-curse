fce9f551633c265bd16d084a49cf817b3f59b5e9
KotenkoNata <kotenkonata8@gmail.com>
2024-04-25 14:49:36 +0200

    feat: Lifting State Up

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,15 +1,25 @@
 import Post from "./Post";
 import styles from "./PostList.module.css";
 import NewPost from "./NewPost";
+import {useState} from "react";
 
 function PostList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [author, setAuthor] = useState('');
 
+    function changeBodyHandler(event) {
+        setEnteredBody(event.target.value)
+    }
+
+    function onAuthorChange(event) {
+        setAuthor(event.target.value)
+    }
 
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={changeBodyHandler} onAuthorChange={onAuthorChange}/>
             <ul className={styles.posts}>
-                <Post author='Max' body='Awesome!' />
+                <Post author={author} body={enteredBody}/>
                 <Post author='Terry' body='Good!' />
             </ul>
         </>
