22c33cd13b7978a64e7f8aacbed2ac3ef7a7279e
KotenkoNata <kotenkonata8@gmail.com>
2024-05-10 15:08:33 +0200

    feat: Styling NextJS Project: Your Options & Using CSS Modules

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cc77c7595bde6eae")
+/******/ 		__webpack_require__.h = () => ("173d2316ea01163f")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7be59442e3360ba4"; }
+/******/ 		__webpack_require__.h = function() { return "2058912de3f0aad9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
