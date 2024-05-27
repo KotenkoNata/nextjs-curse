893617c58420c7da1948db5755f63ca29b64ba71
KotenkoNata <kotenkonata8@gmail.com>
2024-05-27 16:54:42 +0200

    feat: Handling "Not Found" Errors & Showing "Not Found" Pages

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,6 @@
+export default function NewsNotFound() {
+    return <div id="error">
+        <h1>Not found.</h1>
+        <p>Unfortunately, we could not find the requested article.</p>
+    </div>;
+}
