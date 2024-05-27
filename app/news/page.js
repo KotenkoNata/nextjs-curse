3a2a32ba5c90ab92da5b9a104b884cdc18262b48
KotenkoNata <kotenkonata8@gmail.com>
2024-05-27 16:48:38 +0200

    feat: App Styling & Using Dummy Data

--- app/news/page.js
+++ app/news/page.js
@@ -1,19 +1,17 @@
 import Link from "next/link";
+import {DUMMY_NEWS} from "@/dummy-news";
 
 export default function NewsPage() {
     return (
         <>
-            <h1>List of News</h1>
-            <ul>
-                <li>
-                    <Link href='/news/first'>Some link</Link>
-                </li>
-                <li>
-                    <Link href='/news/second'>Some link</Link>
-                </li>
-                <li>
-                    <Link href='/news/third'>Some link</Link>
-                </li>
+            <h1>News Page</h1>
+            <ul className='news-list'>
+                {DUMMY_NEWS.map(newsItem=>(<li key={newsItem.id}>
+                    <Link href={`/news/${newsItem.slug}`}>
+                        <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+                        <span>{newsItem.title}</span>
+                    </Link>
+                </li>))}
             </ul>
         </>
     )
