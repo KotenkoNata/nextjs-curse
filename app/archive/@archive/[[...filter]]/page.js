78a2a00476b1e799d35d2b74d3e643f5faa8a536
KotenkoNata <kotenkonata8@gmail.com>
2024-05-28 15:47:19 +0200

    feat: Catch-All Fallback Routes & Dealing With Multiple Path Segment

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,20 +1,48 @@
-import {getAvailableNewsYears, getNewsForYear} from "@/assets/lib/news";
+import {getAvailableNewsMonths, getAvailableNewsYears, getNewsForYear, getNewsForYearAndMonth} from "@/assets/lib/news";
 import Link from "next/link";
+import NewsList from "@/assets/components/news-list";
 
 export default function FilteredNewsPage({params}) {
     const filter = params.filter;
-    console.log(filter);
-    const links = getAvailableNewsYears();
+    const selectedYear = filter?.[0];
+    const selectedMonth = filter?.[1];
 
-    return <header id="archive-header">
+    let news;
+    let links = getAvailableNewsYears();
+
+    if(selectedYear && !selectedMonth) {
+        news = getNewsForYear(selectedYear);
+        links = getAvailableNewsMonths(selectedYear);
+    }
+
+    if(selectedYear && selectedMonth) {
+        news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+        links = [];
+    }
+
+    let newsContent = <p>No news found for the selected period.</p>
+
+    if(news && news.length > 0){
+        newsContent = <NewsList news={news} />
+    }
+
+
+    return <>
+    <header id="archive-header">
         <nav>
             <ul>
-                {links.map(link => <li key={link}>
-                    <Link href={`/archive/${link}`}>{link}</Link>
-                </li>)}
+                {links.map(link => {
+                    const href = selectedYear
+                        ? `/archive/${selectedYear}/${link}`
+                        : `/archive/${link}`;
+
+                    return <li key={link}>
+                        <Link href={href}>{link}</Link>
+                    </li>
+                })}
             </ul>
         </nav>
     </header>
-    // const news = getNewsForYear(newsYear);
-    // return <NewsList news={news}/>
+    {newsContent}
+    </>
 }
