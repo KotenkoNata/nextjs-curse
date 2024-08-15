6d7ea5daf95543395c3f06fed163f1980dbbf473
KotenkoNata <kotenkonata8@gmail.com>
2024-08-15 13:50:20 +0200

    feat: Showing a Loading Fallback

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,16 +1,13 @@
 import NewsList from "@/assets/components/news-list";
+import { getAllNews } from '@/assets/lib/news'
 
 export default async function NewsPage() {
-    const response = await fetch('http://localhost:8080/news');
-    if(!response.ok){
-      throw new Error(`Failed to fetch news`);
-    }
-    const news = await response.json();
+    const news = await getAllNews()
 
     return (
         <>
             <h1>News Page</h1>
-          <NewsList news={news}/>
+            <NewsList news={news}/>
         </>
     )
 }
